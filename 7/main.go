bf84484dfdf5d5345ca61c7491a4cc5040d1e1dc
Pontalainen <pontusolsson03@gmail.com>
2024-12-07 13:12:23 +0100

    Second half day 7 done

--- 7/main.go
+++ 7/main.go
@@ -11,6 +11,7 @@ func main() {
 	input := readInput()
 
 	firstHalf(input)
+	secondHalf(input)
 }
 
 func readInput() []string {
@@ -30,7 +31,7 @@ func firstHalf(input []string) {
 		numbers := strings.Split(lineParts[1], " ")
 
 		spacesCount := len(numbers) - 1
-		operatorPlacements := generateCombinations("", spacesCount)
+		operatorPlacements := generateCombinations("", spacesCount, 1)
 
 		for _, op := range operatorPlacements {
 			acc, _ := strconv.Atoi(numbers[0])
@@ -54,9 +55,55 @@ func firstHalf(input []string) {
 	fmt.Println(sum)
 }
 
-func generateCombinations(current string, length int) []string {
+func generateCombinations(current string, length int, part int) []string {
 	if len(current) == length {
 		return []string{current}
 	}
-	return append(generateCombinations(current+"*", length), generateCombinations(current+"+", length)...)
+
+	if part == 1 {
+		return append(generateCombinations(current+"*", length, part), generateCombinations(current+"+", length, part)...)
+	}
+
+	return append(
+		append(
+			generateCombinations(current+"*", length, part),
+			generateCombinations(current+"+", length, part)...),
+		generateCombinations(current+"|", length, part)...)
+}
+
+func secondHalf(input []string) {
+	sum := 0
+
+	for _, line := range input {
+		lineParts := strings.Split(line, ": ")
+		testValue, _ := strconv.Atoi(lineParts[0])
+		numbers := strings.Split(lineParts[1], " ")
+
+		spacesCount := len(numbers) - 1
+		operatorPlacements := generateCombinations("", spacesCount, 2)
+
+		for _, op := range operatorPlacements {
+			acc, _ := strconv.Atoi(numbers[0])
+			for i := 0; i < len(numbers)-1; i++ {
+				num, _ := strconv.Atoi(numbers[i+1])
+
+				if op[i] == '*' {
+					acc *= num
+				} else if op[i] == '+' {
+					acc += num
+				} else {
+					accString := strconv.Itoa(acc)
+					accString += strconv.Itoa(num)
+					acc, _ = strconv.Atoi(accString)
+				}
+			}
+
+			if acc == testValue {
+				sum += testValue
+				break
+			}
+		}
+	}
+
+	fmt.Println(sum)
 }
