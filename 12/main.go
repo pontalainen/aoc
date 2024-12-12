c17a473c2e4e52656bff5b916c98e6c5e9ccac28
Pontalainen <pontusolsson03@gmail.com>
2024-12-12 19:35:16 +0100

    Day 12 firstHalf complete

--- 12/main.go
+++ 12/main.go
@@ -24,10 +24,91 @@ func readInput() []string {
 	return strings.Split(string(content), "\n")
 }
 
-func firstHalf(input []string) {
-	fmt.Println(input)
+type Position struct {
+	y, x int
 }
 
-// func secondHalf(input []string) {
+type Region struct {
+	letter string
+	positions []Position
+	perimeter int
+}
 
+func firstHalf(input []string) {
+	regions := getRegions(input)
+	
+	sum := 0
+	for _, region := range regions {
+		regionArea := len(region.positions)
+		regionPerimeter := region.perimeter
+
+		fmt.Println(region.letter, regionArea, regionPerimeter)
+
+		sum += regionArea * regionPerimeter
+	}
+
+	fmt.Println(sum)
+}
+
+func getRegions(grid []string) []Region {
+	regions := []Region{}
+	visited := map[Position]bool{}
+	
+	// Right, Down, Left, Up
+	for y, line := range grid {
+		for x, char := range line {
+			if visited[Position{y, x}] {
+				continue
+			}
+
+			region := floodFill(grid, string(char), y, x, visited)
+			regions = append(regions, region)
+		}
+	}
+
+	return regions
+}
+
+func floodFill(grid []string, letter string, y, x int, visited map[Position]bool, ) Region {
+	dirs := []Position{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	region := Region{letter: letter, positions: []Position{{y, x}}, perimeter: 0}
+	stack := []Position{{y, x}}
+
+	for len(stack) > 0 {
+		pos := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		visited[pos] = true
+		perimeter := 0
+
+		for _, dir := range dirs {
+			newPos := Position{pos.y + dir.y, pos.x + dir.x}
+			if newPos.y < 0 || newPos.y >= len(grid) ||
+				newPos.x < 0 || newPos.x >= len(grid[0]) ||
+				visited[newPos] || string(grid[newPos.y][newPos.x]) != letter {
+					if newPos.y < 0 || newPos.y >= len(grid) ||
+						newPos.x < 0 || newPos.x >= len(grid[0]) ||
+						string(grid[newPos.y][newPos.x]) != letter {
+							perimeter++
+					}
+					continue
+			}
+
+			region.positions = append(region.positions, newPos)
+			stack = append(stack, newPos)
+			visited[newPos] = true
+		}
+
+		region.perimeter += perimeter
+	}
+
+	return region
+}
+
+// func getRegionPerimeter(region Region) int {
+// 	perimeter := 0
+
+// 	for _, pos := range region {
+
+// 	}
 // }
